15db8201e4df644cfd99bc143cd9bdc5b6f22c05
imsourceboom <ecmascript.developer@gmail.com>
2022-01-06 11:17:19 +0900

    modify telegram chat id

--- monitor/alarm.py
+++ monitor/alarm.py
@@ -10,13 +10,13 @@ from random import randint
 
 sleep(randint(1,30))
 
-my_token = '1503237112:AAGQMIrsK_dJpmrp7ulET4g1b15E6rcXs5g'
+my_token = '5081448806:AAH8p4mXI-A5uCKeTchA3l2PeaI1eAvPT1A'
 bot = telegram.Bot(token = my_token)
 HOSTNAME = subprocess.check_output("tail -n 1 ~/serverno",shell=True)
 
 ENGINE=commands.getoutput('~/node.operator.nt/monitor/checkEngine.sh')
 if len(ENGINE) ==0:
-  bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" ENGINE off");
+  bot.sendMessage(chat_id='-1001625520916', text=str(HOSTNAME)+" ENGINE off");
   exit()
 
 VALIDATION = subprocess.check_output("tail -n 1 ~/node.operator.nt/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True);
@@ -31,13 +31,13 @@ SYNCalarm1 = 0
 DISKalarm = 80
 
 if int(VALIDATION) == 0:
-  bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" Not Validating "+str(ERROR_TIME));
+  bot.sendMessage(chat_id='-1001625520916', text=str(HOSTNAME)+" Not Validating "+str(ERROR_TIME));
 if int(ELECTION) == 0:
-  bot.sendMessage(chat_id='-1001225148721', text=str(HOSTNAME)+" Not in Election "+str(ERROR_TIME));
+  bot.sendMessage(chat_id='-1001783489709', text=str(HOSTNAME)+" Not in Election "+str(ERROR_TIME));
 if int(SYNC) < int(SYNCalarm):
-  bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
+  bot.sendMessage(chat_id='-1001625520916', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
 if int(SYNC) > int(SYNCalarm1):
-  bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
+  bot.sendMessage(chat_id='-1001625520916', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
 if int(DISKSPACE) > int(DISKalarm):
-  bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" DISK SPACE FULL, DISK SPACE:"+str(DISKSPACE));
+  bot.sendMessage(chat_id='-1001625520916', text=str(HOSTNAME)+" DISK SPACE FULL, DISK SPACE:"+str(DISKSPACE));
 
